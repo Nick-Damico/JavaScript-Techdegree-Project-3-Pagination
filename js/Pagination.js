cf06629d046cbd6f00935f835b8b17e8cdb444e1
Nick-Damico <nickalan82@icloud.com>
2018-09-15 12:20:25 -0400

    assigning instance attributes in constructor to DOM elements

--- js/Pagination.js
+++ js/Pagination.js
@@ -1,6 +1,9 @@
 class Pagination {
   constructor(resultsPerPage) {
+    this.parent   = document.querySelector('.page');
+    this.students = document.querySelectorAll('.student-item');
 
+    
   }
 }
 
