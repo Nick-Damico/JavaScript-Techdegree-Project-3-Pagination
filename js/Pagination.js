fc3df78047b7e045a79332cbf287da3202e0e46e
Nick-Damico <nickalan82@icloud.com>
2018-09-15 12:23:54 -0400

    create state to keep track of currentPage, number of results to display per page view that is supplied by argument to constructor

--- js/Pagination.js
+++ js/Pagination.js
@@ -3,7 +3,11 @@ class Pagination {
     this._parent   = document.querySelector('.page');
     this._students = document.querySelectorAll('.student-item');
 
-
+    // Set instance state to keep track of the currently viewed page and number of students to filter by
+    this.state = {
+      currentPage: 1,
+      resultsPerPage: resultsPerPage
+    }
   }
 
   getParent() {
