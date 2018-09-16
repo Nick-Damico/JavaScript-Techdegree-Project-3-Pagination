be263ef5108d65550e00f5902e524d3a5cdef4e7
Nick-Damico <nickalan82@icloud.com>
2018-09-15 22:44:45 -0400

    cleaned up order of methods, created #init() method for handling appending, attaching listener

--- js/Pagination.js
+++ js/Pagination.js
@@ -1,3 +1,5 @@
+// Pagination takes optional arguments as a JavaScript Object with key, values to alter behavior of the functionality,
+// If no arguments are provided they will default to values in constructor.
 class Pagination {
   constructor(options={
     numberOfItems: 10,
@@ -6,15 +8,20 @@ class Pagination {
   }) {
     this._parent   = document.querySelector(options.parentSelector);
     this._items = document.querySelectorAll(options.itemSelector);
-    // Bind (instance) to this on method calls
+    // Bind instance to #filterPage method
     this.filterPage = this.filterPage.bind(this);
     // Set instance state to keep track of the currently viewed page and number of students to filter by
     this.state = {
       currentPage: 1,
       resultsPerPage: options.numberOfItems
     }
-    // invoke filterPage() method on initialization of Pagination
-    this.appendPagination();
+
+    this.init();
+  }
+
+  init() {
+    // invoke filterPage(), appendPagination, attach event listener to pagination
+    this.appendPagination().addEventListener('click', this.filterPage);
     this.filterPage();
   }
 
@@ -34,6 +41,14 @@ class Pagination {
     return Math.ceil( this.numberOfItems() / this.state.resultsPerPage );
   }
 
+  appendPagination() {
+    const pagination = this.createPaginationNode();
+    pagination.innerHTML = this.createPaginationNumbers();
+
+    this.getParent().appendChild(pagination);
+    return pagination;
+  }
+
   createPaginationNode() {
     // create element node
     const paginationNode = document.createElement('div');
@@ -47,21 +62,18 @@ class Pagination {
     let paginationNumbers = '';
     // append page numbers to pagaintion
     for (let i = 1; i <= this.numberOfPages(); i++) {
-      paginationNumbers += `<li><a href="">${i}</a></li>`;
+      paginationNumbers += `<li><a href="" data-pgnum="${i}">${i}</a></li>`;
     }
 
     return paginationNumbers;
   }
 
-  appendPagination() {
-    const pagination = this.createPaginationNode();
-    pagination.innerHTML = this.createPaginationNumbers();
-
-    this.getParent().appendChild(pagination);
-  }
-
   filterPage(evt) {
-    evt ? evt.preventDefault() : null;
+    if (evt) {
+      evt.preventDefault();
+      // update state with new current page number using element attr data-pgnum
+      this.updateState({ currentPage: parseInt(evt.target.getAttribute('data-pgnum')) })
+    }
     // calculate range of items that will display, if index of items fall outside of this range hide elements from view.
     const rangeEnd = this.state.currentPage * this.state.resultsPerPage;
     const rangeStart = rangeEnd - this.state.resultsPerPage;
@@ -77,6 +89,14 @@ class Pagination {
     });
 
     return this.getItems();
+  }
+
+  updateState(state) {
+    this.state = {
+      ...this.state,
+      ...state
+    }
+    console.log(this.state);
   }
 }
 
